333b7fb2ab4fa723ef6c38fc8041db5013ca2f5c
Aleksey Litvinov <iamhoneyboy@gmail.com>
2021-08-18 22:13:24 +0100

    properly terminate socket connection after page reload

--- public/bridge.js
+++ public/bridge.js
@@ -1,8 +1,14 @@
 'use strict'
 
 const socket = new window.WebSocket(`ws://${window.location.host}/~/socket/`)
+
 socket.addEventListener('message', handleServerMessage)
+window.addEventListener('beforeunload', closeConnection)
 
 function handleServerMessage (event) {
   if (event.data === 'reload') window.location.reload()
+}
+
+function closeConnection () {
+  socket.close()
 }
